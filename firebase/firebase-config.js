117e7094d3419e57db14f969f82b2bc3d3d5a3ae
N <talktruthjor@gmail.com>
2026-07-19 04:35:01 +0100

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,11 +1,8 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
-
-// Your Firebase Configuration
 
 const firebaseConfig = {
   apiKey: "AIzaSyB1gRwZo8hFRmKl4jNXlyngLWZrCWvQAh0",
@@ -16,8 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "412766918362",
   appId: "1:412766918362:web:c7413e8c1c6664c06db7e3"
 };
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 

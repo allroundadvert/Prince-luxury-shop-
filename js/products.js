b4d8c0f090c7c148338554275ae90eb5ec71dca9
N <talktruthjor@gmail.com>
2026-07-21 06:51:55 +0100

    Implement displayProducts function for product rendering
    Added displayProducts function to render product cards dynamically based on the current category.

--- js/products.js
+++ js/products.js
@@ -67,7 +67,49 @@ Add To Cart
 });
 
 });
+function displayProducts(){
 
+container.innerHTML = "";
+
+allProducts.forEach(product=>{
+
+if(currentCategory !== "All" && product.category !== currentCategory){
+return;
+}
+
+container.innerHTML += `
+
+<div class="product-card">
+
+<img src="${product.image}" alt="${product.name}">
+
+<h3>${product.name}</h3>
+
+<p class="old-price">
+₦${Number(product.oldPrice).toLocaleString()}
+</p>
+
+<h2>
+₦${Number(product.price).toLocaleString()}
+</h2>
+
+<p>${product.description}</p>
+
+<button onclick="shareProduct('${product.name}','${product.price}','${product.description}','${product.image}')">
+📤 Share
+</button>
+
+<button onclick="addToCart('${product.id}')">
+Add To Cart
+</button>
+
+</div>
+
+`;
+
+});
+
+}
 window.addToCart = function(id){
 
 const product = {};
